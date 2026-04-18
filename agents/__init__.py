8e2db5b6b6c6ec9da5f686b8e228e0c85d944bc0
feridecildam <feridecildam@gmail.com>
2026-04-18 14:46:28 +0300

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,13 @@
 from .q_learning import QLearningAgent
-from .dqn import DQNAgent
-from .policy_gradient import PolicyGradientAgent
+
+try:
+    from .dqn import DQNAgent
+except ImportError:
+    DQNAgent = None
+
+try:
+    from .policy_gradient import PolicyGradientAgent
+except ImportError:
+    PolicyGradientAgent = None
 
 __all__ = ["QLearningAgent", "DQNAgent", "PolicyGradientAgent"]
